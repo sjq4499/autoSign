773afae5abb074de457c9ab07dade318c9f878fd
(●´З｀●) <845734834@qq.com>
2022-08-01 09:36:53 +0800

    Update index.js
    添加注释接口返回信息

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const drawFn = async () => {
     method: "POST",
     credentials: "include",
   }).then((res) => res.json());
-
+  // {"err_no":0,"err_msg":"success","data":{"lottery":[{"lottery_id":"6981716980386496552","lottery_name":"随机矿石","lottery_type":1,"lottery_image":"https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/32ed6a7619934144882d841761b63d3c~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image","unlock_count":0},{"lottery_id":"6981716405976743943","lottery_name":"Bug","lottery_type":2,"lottery_image":"https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/0a4ce25d48b8405cbf5444b6195928d4~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image","unlock_count":0},{"lottery_id":"7090359937317994503","lottery_name":"「码赛克」掘金贴纸","lottery_type":4,"lottery_image":"https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/77d5e1e091a944b1b894029d22cdfcf0~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image?","unlock_count":0},{"lottery_id":"7088615500954992644","lottery_name":"「码赛克」线圈毛巾","lottery_type":4,"lottery_image":"https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/77b634472b7f4700a4060edbd8cb3cf3~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image?","unlock_count":0},{"lottery_id":"6981709286694977549","lottery_name":"Yoyo抱枕","lottery_type":3,"lottery_image":"https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/515ebe113d8d407fa5d36191257ce583~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image","unlock_count":0},{"lottery_id":"7107519448088576031","lottery_name":"哔哩哔哩大会员月卡","lottery_type":4,"lottery_image":"https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/defea02506f040849e95cebf350086de~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image?","unlock_count":0},{"lottery_id":"7103338281479176192","lottery_name":"Click午睡枕","lottery_type":3,"lottery_image":"https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/8f0ff284106b44aebf7281900d1707bf~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image?","unlock_count":0},{"lottery_id":"7102706037760720930","lottery_name":"字节咖啡保温杯","lottery_type":3,"lottery_image":"https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/fa31d47d93ce4793a50b664a3e032b18~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image?","unlock_count":0},{"lottery_id":"6981696205310918691","lottery_name":"Switch","lottery_type":3,"lottery_image":"https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/215f7217aa3a48dfa4dbb69976e92b1b~tplv-k3u1fbpfcp-no-mark:0:0:0:0.image","unlock_count":1}],"free_count":0,"point_cost":200}}
   if (draw.err_no !== 0) return Promise.reject("已经签到！免费抽奖异常！");
   console.log(JSON.stringify(draw, null, 2));
   if (draw.data.lottery_type === 1) score += 66;
@@ -73,7 +73,7 @@ const drawFn = async () => {
     method: "POST",
     credentials: "include",
   }).then((res) => res.json());
-
+  // {"err_no":0,"err_msg":"success","data":{"incr_point":100,"sum_point":27712}}
   if (check_in.err_no !== 0) return Promise.reject("签到异常！");
   return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
 })()
@@ -84,6 +84,7 @@ const drawFn = async () => {
       method: "GET",
       credentials: "include",
     }).then((res) => res.json());
+    // {"err_no":0,"err_msg":"success","data":27812}
   })
   .then((res) => {
     console.log(res);
